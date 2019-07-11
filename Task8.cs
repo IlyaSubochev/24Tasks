8d51745b54ec944670aca77f1291f778bbe38474
IlyaSubochev <48482840+IlyaSubochev@users.noreply.github.com>
2019-07-11 14:01:23 +0300

    Update Task8.cs

--- Task8.cs
+++ Task8.cs
@@ -7,59 +7,39 @@ namespace Level1Space
     {
         public static int HowManyTimes(string s, string s_generic)
         {
-            int k = 0;
+            int k = 0;            
             int SeqNumber=0;
             char [] a = s.ToCharArray();
             char[] b = s_generic.ToCharArray();
-            int[] c = new int[a.Length];
-            if (a.Length == 1)
-            {
-                SeqNumber = 0;
-            }
-            else
-            {                  
-                for (int i = 0; i < a.Length; i++)
-                {
-                    k = 0;
-                    for (int j = i; j < b.Length-a.Length+i+1; j++)
-                    {
-                        if (a[i] == b[j])
-                            k++;                      
-                    }
-                    c[i]= k;
-                }
-            }
-            bool SeqFlag = true;
+            int[,] c = new int[a.Length,b.Length];
+            int[] MemC = new int[a.Length*b.Length];
             for (int i = 0; i < a.Length; i++)
-                for (int j = i + 1; j < a.Length; j++)
-                    if (a[i] == a[j])
-                        SeqFlag = false;
-
-            if (c.Length == 2 && a[0] == a[1])
             {
-                SeqNumber = (c[0] - c[1]) * c[1];
-            }
-            else if (c.Length == 2)
-                SeqNumber = c[0] * c[1];
-
-            else if (c[0] * c[1] <= c[c.Length - 1] * c[c.Length - 2] || SeqFlag==true)
-            {
-                SeqNumber = 1;
-                for (int i = 0; i < c.Length; i++)
-                    SeqNumber = SeqNumber * c[i];
-            }
-            else if (c[0] * c[1] > c[c.Length - 1] * c[c.Length - 2] && a.Length<3)
-            {
-                for (int i = 0; i < c.Length; i = i + 2)
+                for (int j = i; j < b.Length; j++)
                 {
-                    if (i < 1)
-                    {
-                        SeqNumber = (c[i] - c[i + 1]) * c[i + 1];
-                    }
+                    if (a[i] == b[j])
+                        c[i, j] = 1;
                     else
-                        SeqNumber = SeqNumber * c[i];
+                        c[i, j] = 0;
                 }
-            } 
+            }
+            for (int j = 0; j < b.Length;j++)
+            {
+                if (c[k, j] == 1 && k < a.Length - 1)
+                {                  
+                    MemC[2 * k + 1] = j;
+                    k++;
+                }
+                else if (k == a.Length - 1 && c[k, j] == 1)
+                    SeqNumber++;                
+                else if (k==0 && j == b.Length - a.Length && c[k, b.Length - a.Length] == 0)
+                    break;
+                if (j == b.Length - 1 && k>0)
+                {
+                    k = k-1;
+                    j = MemC[2 * k + 1];
+                }                
+            }          
             return SeqNumber;
         }
     }
