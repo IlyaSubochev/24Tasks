8ab5be1d8fffbd9de4755e5a4b8ec06982c4e81a
IlyaSubochev <48482840+IlyaSubochev@users.noreply.github.com>
2019-07-11 20:46:06 +0300

    Update Task10.cs

--- Task10.cs
+++ Task10.cs
@@ -9,30 +9,61 @@ namespace Level1Space
         {
             string res = "";
             string ResT = "";
-            double a;
-            double b;
+            string test = "2147483647";
+           
             char[] Str1 = s1.ToCharArray();
-            a = Convert.ToDouble(s1);
-            b = Convert.ToDouble(s2);
-            if (a <= 2147483647 && b <= 2147483647)
+            
+            Console.WriteLine(test.Length);
+            if (s1.Length < 10 && s2.Length < 10)
             {
-                int c = Convert.ToInt32(s1);
-                int d = Convert.ToInt32(s2);
-                int result = c - d;
+                int a = Convert.ToInt32(s1);
+                int b = Convert.ToInt32(s2);
+                int result = a - b;
                 if (result < 0)
                     result = -result;
                 res = Convert.ToString(result);
             }
-            else if (a > 2147483647 && b <= 2147483647)
-            {
-                string s2Res = s2.Substring(s1.Length);
-                int c = Convert.ToInt32(s1);
-                int d = Convert.ToInt32(s2Res);
-                int result = d - c;
-                ResT= Convert.ToString(result);
-                res = s2.Remove(s2.Length-s1.Length);
-                res = res.Insert(res.Length, ResT);
-            }
+            
+            //if (a == b)
+            //    res = "0";
+            //else if (a <= 2147483647 && b <= 2147483647)
+            //{
+            //    int c = Convert.ToInt32(s1);
+            //    int d = Convert.ToInt32(s2);
+            //    int result = c - d;
+            //    if (result < 0)
+            //        result = -result;
+            //    res = Convert.ToString(result);
+            //}
+            //else if (a > 2147483647 && b <= 2147483647)
+            //{
+            //    string s2Res = s2.Substring(s1.Length);
+            //    int c = Convert.ToInt32(s1);
+            //    int d = Convert.ToInt32(s2Res);
+            //    int result = c - d;
+            //    if (result < 0)
+            //        result = -result;
+            //    ResT = Convert.ToString(result);
+            //    res = s2.Remove(s2.Length - s1.Length);
+            //    res = res.Insert(res.Length, ResT);
+            //}
+            //else if (b > 2147483647 && a <= 2147483647)
+            //{
+            //    string s2Res = s1.Substring(s2.Length);
+            //    int c = Convert.ToInt32(s1);
+            //    int d = Convert.ToInt32(s2Res);
+            //    int result = c - d;
+            //    if (result < 0)
+            //        result = -result;
+            //    ResT = Convert.ToString(result);
+            //    res = s2.Remove(s2.Length - s1.Length);
+            //    res = res.Insert(res.Length, ResT);
+            //}
+            //else if (a > 2147483647 && b > 2147483647 && a>b)
+            //{
+            //    res = "dads";
+            //}
+
             return res;
         }
     }
