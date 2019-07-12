c6d2152daab43d28befee3334a30a136bb28b0e8
IlyaSubochev <48482840+IlyaSubochev@users.noreply.github.com>
2019-07-12 15:37:20 +0300

    Update Task10.cs

--- Task10.cs
+++ Task10.cs
@@ -5,16 +5,21 @@ namespace Level1Space
 {
     public static class Level1
     {
+        public static int[] ArrConvert(string s)
+        {
+            int[] Arr = new int[2];                           
+            Arr[0] = Convert.ToInt32(s.Substring(0,s.Length - s.Length / 2));
+            Arr[1] = Convert.ToInt32(s.Substring(s.Length - s.Length / 2));
+            return Arr;
+        }
         public static string BigMinus(string s1, string s2)
         {
             string res = "";
-            string ResT = "";
-            string test = "2147483647";
-           
             char[] Str1 = s1.ToCharArray();
-            
-            Console.WriteLine(test.Length);
-            if (s1.Length < 10 && s2.Length < 10)
+            char[] Str2 = s2.ToCharArray();          
+            if (s1.Equals(s2) == true)
+                res = "0";
+            else if (s1.Length < 10 && s2.Length < 10)
             {
                 int a = Convert.ToInt32(s1);
                 int b = Convert.ToInt32(s2);
@@ -23,47 +28,122 @@ namespace Level1Space
                     result = -result;
                 res = Convert.ToString(result);
             }
-            
-            //if (a == b)
-            //    res = "0";
-            //else if (a <= 2147483647 && b <= 2147483647)
-            //{
-            //    int c = Convert.ToInt32(s1);
-            //    int d = Convert.ToInt32(s2);
-            //    int result = c - d;
-            //    if (result < 0)
-            //        result = -result;
-            //    res = Convert.ToString(result);
-            //}
-            //else if (a > 2147483647 && b <= 2147483647)
-            //{
-            //    string s2Res = s2.Substring(s1.Length);
-            //    int c = Convert.ToInt32(s1);
-            //    int d = Convert.ToInt32(s2Res);
-            //    int result = c - d;
-            //    if (result < 0)
-            //        result = -result;
-            //    ResT = Convert.ToString(result);
-            //    res = s2.Remove(s2.Length - s1.Length);
-            //    res = res.Insert(res.Length, ResT);
-            //}
-            //else if (b > 2147483647 && a <= 2147483647)
-            //{
-            //    string s2Res = s1.Substring(s2.Length);
-            //    int c = Convert.ToInt32(s1);
-            //    int d = Convert.ToInt32(s2Res);
-            //    int result = c - d;
-            //    if (result < 0)
-            //        result = -result;
-            //    ResT = Convert.ToString(result);
-            //    res = s2.Remove(s2.Length - s1.Length);
-            //    res = res.Insert(res.Length, ResT);
-            //}
-            //else if (a > 2147483647 && b > 2147483647 && a>b)
-            //{
-            //    res = "dads";
-            //}
+            else if (s1.Length >= 10 && s2.Length >= 10)
+            {
+                int[] ArrS1 = ArrConvert(s1);
+                int[] ArrS2 = ArrConvert(s2);
+                int[] ArrRes = new int[2];
+                if (ArrS1.Length == ArrS2.Length)
+                {
+                    for (int i = ArrS2.Length - 1; i >= 0; i--)
+                    {
+                        ArrRes[i] = ArrS1[i] - ArrS2[i];
+                    }
+                    for (int i = ArrRes.Length - 1; i >= 0; i--)
+                    {
+                        if (ArrRes[i] < 0)
+                        {
+                            ArrRes[i] = -ArrRes[i];
+                        }
+                    }
+                }
+                for (int i = 0; i < ArrRes.Length; i++)
+                {
+                    int t = ArrRes[i];
+                    res = res + Convert.ToString(t);
+                }
+                if (s1.Length > s2.Length)
+                {
+                    while (res.Length != s1.Length)
+                        res = res.Insert(res.Length, "0");
+                }
+                else
+                {
+                    while (res.Length != s2.Length)
+                        res = res.Insert(res.Length, "0");
+                }
+            }
+            else if (s1.Length >= 10 && s2.Length < 10)
+            {
+                int[] ArrS1 = ArrConvert(s1);
+                int[] ArrRes = new int[2];
+                if ((s1.Length / 2) >= s2.Length)
+                {
+                    int ArrS2 = Convert.ToInt32(s2);
+                    ArrRes[0] = ArrS1[0];
+                    ArrRes[1] = ArrS1[1] - ArrS2;
+                    for (int i = 0; i < ArrRes.Length; i++)
+                    {
+                        int t = ArrRes[i];
+                        res = res + Convert.ToString(t);
+                    }
+                }
+                else
+                {
+                    int[] ArrS2 = new int[2];
+                    ArrS2[0] = Convert.ToInt32(s2.Substring(0, s2.Length - s1.Length / 2));
+                    ArrS2[1] = Convert.ToInt32(s2.Substring(s2.Length - s1.Length / 2));
+                    for (int i = 0; i < ArrRes.Length; i++)
+                    {
+                        ArrRes[i] = ArrS1[i] - ArrS2[i];
+                    }
+                    for (int i = 0; i < ArrRes.Length; i++)
+                    {
+                        if (ArrRes[i] < 0)
+                        {
+                            ArrRes[i] = -ArrRes[i];
+                        }
+                    }
+                    for (int i = 0; i < ArrRes.Length; i++)
+                    {
+                        int t = ArrRes[i];
+                        res = res + Convert.ToString(t);
+                    }
+                    while (res.Length != s1.Length)
+                        res = res.Insert(res.Length, "0");
 
+                }
+            }
+            else if (s2.Length >= 10 && s1.Length < 10)
+            {
+                int[] ArrS2 = ArrConvert(s2);
+                int[] ArrRes = new int[2];
+                if ((s2.Length / 2) >= s1.Length)
+                {
+                    int ArrS1 = Convert.ToInt32(s1);
+                    ArrRes[0] = ArrS2[0];
+                    ArrRes[1] = ArrS2[1] - ArrS1;
+                    for (int i = 0; i < ArrRes.Length; i++)
+                    {
+                        int t = ArrRes[i];
+                        res = res + Convert.ToString(t);
+                    }
+                }
+                else
+                {
+                    int[] ArrS1 = new int[2];
+                    ArrS1[0] = Convert.ToInt32(s1.Substring(0, s1.Length - s2.Length / 2));
+                    ArrS1[1] = Convert.ToInt32(s1.Substring(s1.Length - s2.Length / 2));
+                    for (int i = 0; i < ArrRes.Length; i++)
+                    {
+                        ArrRes[i] = ArrS2[i] - ArrS1[i];
+                    }
+                    for (int i = 0; i < ArrRes.Length; i++)
+                    {
+                        if (ArrRes[i] < 0)
+                        {
+                            ArrRes[i] = -ArrRes[i];
+                        }
+                    }
+                    for (int i = 0; i < ArrRes.Length; i++)
+                    {
+                        int t = ArrRes[i];
+                        res = res + Convert.ToString(t);
+                    }
+                    while (res.Length != s2.Length)
+                        res = res.Insert(res.Length, "0");
+                }
+            }
             return res;
         }
     }
