17baab131029e07e36e9eb93513e077744e51961
IlyaSubochev <48482840+IlyaSubochev@users.noreply.github.com>
2019-07-13 14:49:59 +0300

    Update Task11.cs

--- Task11.cs
+++ Task11.cs
@@ -14,24 +14,28 @@ namespace Level1Space
             string res = "";
             double ValueVotes = 0;
             double max;
-            int k = 1;
+            int k = 0;
             double[] PerVotes = new double[N];
             for (int i = 0; i < N; i++)
                 ValueVotes = ValueVotes + Votes[i];
             for (int i = 0; i < N; i++)
                 PerVotes[i] = MyRound((Votes[i] * 100) / ValueVotes,2);
             max = PerVotes[0];
+            int maxFlag = 0;
             for (int i = 0; i < N; i++)
             {
-                if (max < PerVotes[i])
+                if (max <= PerVotes[i])
                 {
                     k = i + 1;
                     max = PerVotes[i];
+                    maxFlag++;
                 }
             }
-            if (max > 50)
+            if (max > 50 && maxFlag==1)
                 res = "majority winner " + k;
-            else if (max < 50 && max > 40)
+            else if ((max > 50 && maxFlag > 1) || (max < 50 && max > 40 && maxFlag > 1))
+                res = "no winner";
+            else if (max < 50 && max > 40 && maxFlag == 1)
                 res = "minority winner " + k;
             else
                 res = "no winner";
