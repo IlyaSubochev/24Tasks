08d74e43ae44f76892a927534ce5d7464e7f9299
IlyaSubochev <48482840+IlyaSubochev@users.noreply.github.com>
2019-07-15 12:27:47 +0300

    Create Task13.cs

---
+++ Task13.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static int Unmanned(int L, int N, int[][] track)
+        {
+            int result = 0;
+            int DistL = 0;
+            int i = 0;
+            for (int j = 1; j < N; j++)
+            {
+                int[] TempTrack = new int[3];
+                
+                if (track[0][0] > track[j][0])
+                {
+                    TempTrack = track[j];
+                    track[j] = track[0];
+                    track[0] = TempTrack;
+                }
+            }
+            for (int j=0;j<3;j++) Console.WriteLine(track[0][j]);
+                while (DistL <= L)
+                {
+                if (i == N)
+                {
+                    result = result + L - DistL;
+                    DistL = L + 1;
+                }
+                else
+                {
+                    if (i < N && DistL == track[i][0])
+                    {
+                        if (N > 1 && DistL == track[i][0] && result == (track[0][1] + track[1][1]))
+                        {
+                            DistL++;
+                            result++;
+                        }
+                        else if (DistL < track[i][1])
+                        {
+                            result = result + track[i][1] - DistL;
+                            DistL++;
+                        }
+                        else if (DistL > track[i][1] && result > track[i][1] && result % (track[i][1] * 2) < 5)
+                        {
+                            result = result + (track[i][1] - (result - track[i][1] * (result / track[i][1])));
+                            DistL++;
+                        }
+                        //&& result > track[i][1] && result % (track[i][1] * 2) < 5
+                        else if (DistL > track[i][1] && result < track[i][1] && result % (track[i][1] * 2) < 5)
+                        {
+                            result = result + (track[i][1] - result);
+                            DistL++;
+                        }
+                        i++;
+                    }
+                    else
+                    {
+                        DistL++;
+                        result++;
+                    }
+                }
+                   
+            }          
+            return result;
+        }
+    }
+}
