2954492d974182b1db6c592f837d346e80ac8aca
IlyaSubochev <48482840+IlyaSubochev@users.noreply.github.com>
2019-07-16 10:06:20 +0300

    Update Task13.cs

--- Task13.cs
+++ Task13.cs
@@ -20,52 +20,42 @@ namespace Level1Space
                     track[j] = track[0];
                     track[0] = TempTrack;
                 }
-            }
+            }     
             while (DistL <= L)
             {
                 if (i == N)
                 {
-
                     result = result + L - DistL;
                     DistL = L + 1;
                 }
+                else if (i < N && DistL == track[i][0])
+                {
+                    if (DistL < track[i][1])
+                        result = result + track[i][1] - DistL;
+                    else if (DistL > track[i][1] && DistL < track[i][1] + track[i][2])
+                    {
+                        result++;
+                        DistL++;
+                    }
+                    else if (result%(track[i][1] + track[i][2]) > track[i][1] && 
+                            result % (track[i][1] + track[i][2]) < track[i][1] + track[i][2])
+                    {
+                        result++;
+                        DistL++;
+                    }
+                    else if (result % (track[i][1] + track[i][2]) < track[i][1])
+                    {
+                        result = result + (track[i][1] - (result - (track[i][1] + track[i][2]) * (result / (track[i][1] + track[i][2]))));
+                    }
+                    i++;
+                }
+                else if (DistL == L && track[track.Length - 1][0] != L)
+                    DistL = DistL + 1;
                 else
                 {
-                    if (DistL == L && track[track.Length-1][0] != L)                   
-                        DistL = DistL+1;
-                    else if (i < N && DistL == track[i][0])
-                    {
-                        if (N > 1 && DistL == track[i][0] && result == (track[0][1] + track[1][1]))
-                        {
-                            DistL++;
-                            result++;
-                        }
-                        else if (DistL < track[i][1])
-                        {
-                            result = result + track[i][1] - DistL;
-                            //DistL++;
-                        }
-                        else if (DistL > track[i][1] && result > track[i][1] && result % (track[i][1] + track[i][2]) < 5)
-                        {
-                            result = result + (track[i][1] - (result - track[i][1] * (result / track[i][1])));
-                            
-                            DistL++;
-                        }
-                        else if (DistL > track[i][1] && result < track[i][1] && result % (track[i][1] + track[i][2]) < 5)
-                        {
-                            result = result + (track[i][1] - result);
-                            //DistL++;
-                        }
-                        i++;
-                        
-                    }
-                    else
-                    {
-                        DistL++;
-                        result++;
-                    }
-                }
-
+                    DistL++;
+                    result++;
+                }                
             }
             return result;
         }
